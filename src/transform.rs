f190294c2d9d1e0778427b28b6fa05a140073095
Nick Cao <nickcao@nichi.co>
2025-12-15 22:08:07 -0500

    tranform type 4

--- src/transform.rs
+++ src/transform.rs
@@ -157,3 +157,75 @@ pub enum AUTH {
     #[deku(id_pat = "1024..=65535")]
     Private(u16),
 }
+
+/// Transform Type 4 - Key Exchange Method Transform IDs
+/// Reference: https://www.iana.org/assignments/ikev2-parameters/ikev2-parameters.xhtml#ikev2-parameters-8
+#[allow(non_camel_case_types)]
+#[derive(Clone, Debug, PartialEq, DekuRead, DekuWrite)]
+#[deku(id_type = "u16", endian = "endian", ctx = "endian: deku::ctx::Endian")]
+pub enum KE {
+    #[deku(id = 0)]
+    NONE,
+    #[deku(id = 1)]
+    MODP_768,
+    #[deku(id = 2)]
+    MODP_1024,
+    #[deku(id_pat = "3..=4")]
+    Reserved0(u16),
+    #[deku(id = 5)]
+    MODP_1536,
+    #[deku(id_pat = "6..=13")]
+    Unassigned0(u16),
+    #[deku(id = 14)]
+    MODP_2048,
+    #[deku(id = 15)]
+    MODP_3072,
+    #[deku(id = 16)]
+    MODP_4096,
+    #[deku(id = 17)]
+    MODP_6144,
+    #[deku(id = 18)]
+    MODP_8192,
+    #[deku(id = 19)]
+    ECP_256,
+    #[deku(id = 20)]
+    ECP_384,
+    #[deku(id = 21)]
+    ECP_512,
+    #[deku(id = 22)]
+    MODP_1024_PRIME_160,
+    #[deku(id = 23)]
+    MODP_2048_PRIME_224,
+    #[deku(id = 24)]
+    MODP_2048_PRIME_256,
+    #[deku(id = 25)]
+    ECP_192,
+    #[deku(id = 26)]
+    ECP_224,
+    #[deku(id = 27)]
+    brainpoolP224r1,
+    #[deku(id = 28)]
+    brainpoolP256r1,
+    #[deku(id = 29)]
+    brainpoolP384r1,
+    #[deku(id = 30)]
+    brainpoolP512r1,
+    #[deku(id = 31)]
+    Curve25519,
+    #[deku(id = 32)]
+    Curve448,
+    #[deku(id = 33)]
+    GOST3410_2012_256,
+    #[deku(id = 34)]
+    GOST3410_2012_512,
+    #[deku(id = 35)]
+    ML_KEM_512,
+    #[deku(id = 36)]
+    ML_KEM_768,
+    #[deku(id = 37)]
+    ML_KEM_1024,
+    #[deku(id_pat = "38..=1023")]
+    Unassigned1(u16),
+    #[deku(id_pat = "1024..=65535")]
+    Private(u16),
+}
